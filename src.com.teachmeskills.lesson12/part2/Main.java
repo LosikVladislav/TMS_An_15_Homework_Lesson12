12acabe6683b2b5ae05200155f43a1bdefec2ffe
losikvladislav <vlad.losik@yandex.ru>
2023-09-10 18:02:35 +0300

    hw12

---
+++ src.com.teachmeskills.lesson12/part2/Main.java
@@ -0,0 +1,25 @@
+package part2;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Vladislav Losik");
+        set.add("Appleice");
+        set.add("Gleb Urchuk");
+        set.add("Alex Gorbal");
+        set.add("Dima Kripan");
+        set.add("TeachMeSkills");
+        set.add("Dovlet");
+        set.add("AndreichD");
+        set.add("Tasha");
+        set.add("Roman Belov");
+        set.add("Andrey");
+        set.add("Alex Grouzdev");
+
+        System.out.println(set);
+    }
+}
